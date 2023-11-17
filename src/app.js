3de1a61821fbc858ed9d62cf6005b0d2f969be02
jackievivianv <145908828+jackievivianv@users.noreply.github.com>
2023-11-17 20:39:09 +0000

    final de random card

--- src/app.js
+++ src/app.js
@@ -5,7 +5,47 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function jugar() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const cardsNumbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const cardsPalos = ["♦", "♥", "♠", "♣"];
+
+  const randomCards = Math.floor(Math.random() * cardsNumbers.length);
+  const randomPalos = Math.floor(Math.random() * cardsPalos.length);
+
+  document.getElementById("numero").innerHTML = cardsNumbers[randomCards];
+
+  if (randomPalos === 0 || randomPalos === 1) {
+    document.getElementById(
+      "iconosPalos"
+    ).innerHTML = `<span class="text-danger"> ${cardsPalos[randomPalos]} </span>`;
+    document.getElementById(
+      "iconosPalosBajo"
+    ).innerHTML = `<span class="text-danger"> ${cardsPalos[randomPalos]} </span>`;
+  } else {
+    document.getElementById("iconosPalos").innerHTML = cardsPalos[randomPalos];
+    document.getElementById("iconosPalosBajo").innerHTML =
+      cardsPalos[randomPalos];
+  }
+
+  let button = document.getElementById("boton");
+
+  button.addEventListener("click", function() {
+    jugar();
+  });
 };
